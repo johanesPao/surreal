c737dbcae01f1257079cb7bebcc84353d2a9c1a5
Johanes Gregorius Indra Pradana Pao <johanes.pao@pri.co.id>
2024-06-20 10:59:58 +0700

    Hooks window pada komponen syntaxhighlight

--- komponen/mdx/syntaxhighlight.tsx
+++ komponen/mdx/syntaxhighlight.tsx
@@ -1,7 +1,8 @@
 'use client'
 
+import { useState, useEffect } from 'react'
 import { Prism as SyntaxHighlighter} from 'react-syntax-highlighter'
-import {gruvboxDark} from 'react-syntax-highlighter/dist/esm/styles/prism'
+import { gruvboxDark } from 'react-syntax-highlighter/dist/esm/styles/prism'
 
 
 type SyntaxProps = {
@@ -10,23 +11,51 @@ type SyntaxProps = {
 }
 
 const SyntaxHighlight = ({...props}: SyntaxProps) => {
+    const [diDesktop, setDiDesktop] = useState<boolean|undefined>(undefined)
+    const [lebarWindow, setLebarWindow] = useState<number|undefined>(undefined)
     const { bahasa, kode } = props
+
+    useEffect(() => {
+        if (typeof window !== 'undefined') {
+            // Inisiasi lebarWindow pada render pertama
+            setLebarWindow(window.innerWidth)
+            // Inisiasi diDekstop pada render pertama
+            if (window.innerWidth >= 1024) {
+                setDiDesktop(true)
+            } else {
+                setDiDesktop(false)
+            }
+            // Registrasi event listener untuk resize saat render pertama
+            window.addEventListener('resize', () => {
+                setLebarWindow(window.innerWidth)
+            })
+        }
+    }, [])
+
+    useEffect(() => {
+        if (typeof window !== 'undefined') {
+            // Evaluasi lebarWindow pada saat terjadi perubahan pada lebarWindow
+            lebarWindow && lebarWindow >= 1024
+                ? setDiDesktop(true)
+                : setDiDesktop(false)
+        }
+    }, [lebarWindow])
 
     return (
         <div className="flex flex-col">
             <div className="flex flex-row px-[5%] lg:px-[20%] w-0">
-                <span className="px-4 py-0.5 bg-[#282828] text-lg rounded-tl-lg rounded-tr-lg">TSX</span>
+                <span className="px-4 py-0.5 bg-[#282828] text-sm italic rounded-tl-lg rounded-tr-lg">{bahasa.toUpperCase()}</span>
             </div>
             <SyntaxHighlighter 
                 customStyle={{
                     margin: '0',
                     background: '#282828',
-                    paddingLeft: '5%',
-                    paddingRight: '5%'
+                    paddingLeft: diDesktop ? '20%' : '5%',
+                    paddingRight: diDesktop ? '20%' : '5%',
                 }}
                 language={bahasa} 
                 style={gruvboxDark} 
-                className="text-xs mt-0 shadow-lg"
+                className="text-xs mt-0 shadow-lg px-[5%] lg:px-[20%]"
                 showLineNumbers
             >
                 {kode}
