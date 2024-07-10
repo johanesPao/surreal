bbd588ee36ab7c07b8dda07a314319f5de6c6414
Johanes Gregorius Indra Pradana Pao <johanes.pao@pri.co.id>
2024-07-10 17:22:26 +0700

    Import font on konten layout

--- komponen/mdx/excelline.tsx
+++ komponen/mdx/excelline.tsx
@@ -5,7 +5,7 @@ type ExcelLineProps = {
 const ExcelLine = ({ children }: ExcelLineProps) => {
   return (
     <span className='relative inline-block before:block before:absolute before:-inset-1 before:-skew-y-1 before:bg-[#339966] before:opacity-70 before:rounded-s -z-5'>
-      <span className='relative text-slate-200 font-mono text-sm italic'>
+      <span className='relative text-slate-200 font-inconsolata text-sm italic'>
         {children}
       </span>
     </span>
