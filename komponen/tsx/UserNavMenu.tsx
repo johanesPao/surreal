1d3e977d459c988d40d78117cb373be02be65a9b
Johanes Gregorius Indra Pradana Pao <johanes.pao@pri.co.id>
2024-08-09 17:43:33 +0700

    cursor-not-allowed for UserNavMenu as placeholder for now

--- komponen/tsx/UserNavMenu.tsx
+++ komponen/tsx/UserNavMenu.tsx
@@ -6,8 +6,8 @@ const UserNavMenu = () => {
   return (
     <div className="flex flex-col gap-12 items-center text-stone-400 text-sm">
       <div className="flex flex-col gap-3 items-center">
-        <span>Comments</span>
-        <span>Social accounts</span>
+        <span className="cursor-not-allowed">Comments</span>
+        <span className="cursor-not-allowed">Social accounts</span>
       </div>
       <LogOutButton />
     </div>
